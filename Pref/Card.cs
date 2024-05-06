828b3f277f45594f78ad175ee193862696dce9ec
alexander.rozov <alexander.rozov>
2024-05-07 02:33:44 +0400

    [functionality] Card

---
+++ Pref/Card.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pref
+{
+    public class Card
+    {
+
+        public string Suit { get; set; }
+        public string Value { get; set; }
+
+        public Card(string s, string v) 
+        {
+            Suit = s;
+            Value = v;
+        }
+
+
+    }
+}
---
+++ Pref/Deck.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pref
+{
+    internal class Deck
+    {
+        uint capacity;
+        public List<Card> CardsInDeck { get; set; }
+        internal enum DeckType {Full, RussianType, PreferanceType}
+
+        public string[] CardSuit = { "Spades", "Clubs", "Diamonds", "Hearts" };
+        public string[] CardValue = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" };
+
+        internal DeckType deck_type { get; set; }
+        internal Deck(DeckType deckType) 
+        {
+            CardsInDeck = new List<Card>();
+            deck_type = deckType;
+
+            switch (deckType)
+            {
+
+                case DeckType.Full:
+                    capacity = 52;
+                    break;
+                case DeckType.RussianType:
+                    capacity = 36;
+                    break;
+
+                case DeckType.PreferanceType:
+                    capacity = 32;
+                    break;
+                 default:
+                 capacity = 52;
+                 break;
+            }            
+        }
+        internal void InitDeck()
+        {
+            foreach (var suit in CardSuit)
+            {
+                foreach (var value in CardValue)
+                {
+                    CardsInDeck.Add(new Card(suit, value));
+                }
+            }
+            
+            if (deck_type == DeckType.RussianType)
+            {
+               
+            }
+            if (deck_type == DeckType.PreferanceType)
+            {
+
+            }
+        }
+    }
+}
